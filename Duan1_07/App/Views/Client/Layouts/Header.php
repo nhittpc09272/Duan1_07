7efa2e7db34f43e03890cef89460bbd0620ff941
Thanh <you@example.com>
2024-11-19 23:34:07 +0700

    Cập nhật trang Sản phẩm

--- Duan1_07/App/Views/Client/Layouts/Header.php
+++ Duan1_07/App/Views/Client/Layouts/Header.php
@@ -33,6 +33,14 @@ class Header extends BaseView
             <link href="/public/assets/client/css/bootstrap.min.css" rel="stylesheet">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+            <link rel="stylesheet" href="/public/assets/client/css/linearicons.css">
+            <link rel="stylesheet" href="/public/assets/client/css/owl.carousel.css">
+            <link rel="stylesheet" href="/public/assets/client/css/font-awesome.min.css">
+            <link rel="stylesheet" href="/public/assets/client/css/themify-icons.css">
+            <link rel="stylesheet" href="/public/assets/client/css/nice-select.css">
+            <link rel="stylesheet" href="/public/assets/client/css/nouislider.min.css">
+            <link rel="stylesheet" href="/public/assets/client/css/bootstrap.css">
+            <link rel="stylesheet" href="/public/assets/client/css/main.css">
         </head>
         <style>
             :root {
