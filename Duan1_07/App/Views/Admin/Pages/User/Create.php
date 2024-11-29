e9faf58091eb596ac24af5d0e8a4edfd4f4e04b5
NHT05 <nhutnguyentrung449@gmail.com>
2024-11-29 15:17:02 +0700

    update admin(carete)

---
+++ Duan1_07/App/Views/Admin/Pages/User/Create.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Views\Admin\Pages\User;
+
+use App\Views\BaseView;
+
+class Create extends BaseView
+{
+    public static function render($data = null)
+    {
+
+?>
+
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-12 d-flex no-block align-items-center">
+                        <h4 class="page-title">QUẢN LÝ NGƯỜI DÙNG</h4>
+                        <div class="ms-auto text-end">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="/admin">Trang chủ</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Thêm người dùng</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <form class="form-horizontal" action="/admin/users" method="POST" enctype="multipart/form-data">
+                            <div class="card-body">
+                                    <h4 class="card-title">Thêm người dùng</h4>
+                                    <input type="hidden" name="method" id="" value="POST">
+                                    <div class="form-group">
+                                        <label for="username">Tên đăng nhập*</label>
+                                        <input type="text" class="form-control" id="username" placeholder="Nhập tên đăng nhập..." name="username" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email">Email*</label>
+                                        <input type="text" class="form-control" id="email" placeholder="Nhập tên email..." name="email" >
+                                    </div>
+                                    <!-- <div class="form-group">
+                                        <label for="name">Tên*</label>
+                                        <input type="text" class="form-control" id="name" placeholder="Nhập họ và tên người dùng..." name="name" >
+                                    </div>  -->
+                                    <div class="form-group">
+                                        <label for="password">Mật khẩu*</label>
+                                        <input type="password" class="form-control" id="password" placeholder="Nhập mật khẩu..." name="password" >
+                                    </div> 
+                                    
+                                    <div class="form-group">
+                                        <label for="ar">Nhập lại mật khẩu*</label>
+                                        <input type="password" class="form-control" id="re_password" placeholder="Nhập lại mật khẩu..." name="re_password" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="phone">số điện thoại*</label>
+                                        <input type="number" class="form-control" id="phone" placeholder="Nhập lại mật khẩu..." name="phone" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="address">address*</label>
+                                        <input type="text" class="form-control" id="address" placeholder="Nhập lại mật khẩu..." name="address" >
+                                    </div>
+                                 <div class="form-group">
+                                        <label for="avatar">Ảnh đại diện</label>
+                                        <input type="file" class="form-control" id="avatar" placeholder="Chọn ảnh đại diện..." name="avatar" >
+                                    </div>
+                                 
+                                    <div class="form-group">
+                                        <label for="status">Trạng thái*</label>
+                                        <select class="select2 form-select shadow-none" style="width: 100%; height:36px;" id="status" name="status" >
+                                            <option value="" selected disabled>Vui lòng chọn...</option>
+                                            <option value="1">Hiển thị</option>
+                                            <option value="0">Ẩn</option>
+
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="border-top">
+                                    <div class="card-body">
+                                        <button type="reset" class="btn btn-danger text-white" name="">Làm lại</button>
+                                        <button type="submit" class="btn btn-primary" name="">Thêm</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+
+            <div class="form-group">
+                <?php if (isset($_SESSION['error'])): ?>
+                    <div class="alert alert-danger">
+                        <?php
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']); // Xóa sau khi hiển thị để tránh hiển thị lại
+                        ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+
+    <?php
+    }
+}
